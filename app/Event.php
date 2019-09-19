520716ab065ad1b358d2e45401e80ec0e6372ed6
Buthaina <buthainasudi@gmail.com>
2019-09-20 00:46:41 +0800

    Creating Event table, model and controller

---
+++ app/Event.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str as generate;
+
+class Event extends Model
+{
+    public $incrementing = false;
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'title', 'description', 'date','venue',
+    ];
+
+    public static function boot(){
+        parent::boot();
+
+        static::creating(function($user){
+            $user->id = (string) generate::uuid();
+        });
+    }
+}
