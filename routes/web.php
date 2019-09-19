327581995dad4551877fbd40f56abe8534986d7c
Buthaina <buthainasudi@gmail.com>
2019-09-20 02:36:10 +0800

    Change home page to events.index page

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'EventController@index')->name('home');
 Route::resource('events','EventController');
