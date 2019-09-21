6499c6347e9130537d6f47641876c995967076c8
Buthaina <buthainasudi@gmail.com>
2019-09-21 19:46:13 +0800

    Add comment controller

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\File;
+
+use App\Http\Requests;
+use App\Comment;
+use App\Event;
+use Auth;
+
+class CommentController extends Controller
+{
+
+
+}
