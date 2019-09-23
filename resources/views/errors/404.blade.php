4ddb34a9efe726680363e64ce392fabbef198f6c
Buthaina <buthainasudi@gmail.com>
2019-09-23 22:57:33 +0800

    Customize 404 Error Page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,6 @@
-@extends('errors::minimal')
+@extends('layouts.app')
 
-@section('title', __('Not Found'))
-@section('code', '404')
-@section('message', __('Not Found'))
+@section('content')
+    <p>Sorry, Page Not Found</p>
+@endsection
+
