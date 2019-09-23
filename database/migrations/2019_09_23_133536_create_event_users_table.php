3183d1899d09db22482ac46accbb08ad89ba866e
Buthaina <buthainasudi@gmail.com>
2019-09-23 21:53:27 +0800

    Create a migration for event-user intermediate table

---
+++ database/migrations/2019_09_23_133536_create_event_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event_users', function (Blueprint $table) {
+            $table->uuid('user_id');
+            $table->uuid('event_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::rename('event_users', 'event_user');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('event_users');
+    }
+}
