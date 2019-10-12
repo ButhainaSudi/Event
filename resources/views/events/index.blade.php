ccf4771c3092d33ad1a93fd43eed6aafdd3e27bf
Buthaina <buthainasudi@gmail.com>
2019-10-12 14:46:38 +0800

    Change the UI

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -1,133 +1,181 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-<div class="row justify-center">
-    <div class="col-md-10">
+<br><br>
+<!-- Navigation bar with social media icons -->
 
-                    
+  
+<!-- w3-content defines a container for fixed size centered content, 
+and is wrapped around the whole page content, except for the footer in this example -->
+<div class="w3-content" style="max-width:1600px">
 
-                <div class="row"> 
-                <div class="w3-row-padding w3-theme">
-                    @foreach($event as $event => $value)                     
-                    <div class="w3-col s12 m12 l6 w3-section">
-                        <div class="w3-card-4">
-                                <div class="w3-container w3-white">
-                                    <br><h4><strong>{{ $value->title }}</strong></h4>
-                                    <p>{{ $value->description }}</p>
-                                    <p>Date <strong>{{ $value->date }}</strong></p>
-                                    <p>Venue <strong>{{ $value->venue }}</strong></p>
+  <!-- Header -->
+  <header class="w3-container w3-center w3-padding-48 w3-white">
+    <h1 class="w3-xxxlarge"><b><span class="w3-tag">EVENTS</span></b></h1>  
+  </header>
 
-                                    <div class="login100-pic js-tilt" data-tilt>
-                                        <img src="{{url($value->image? 'uploads/'.$value->image:'images/noimage.jpg')}}" style="width:100%">
-                                    </div><br><hr>
 
-                                    <?php
-                                        //$event = App\User::find(1);
-                                        $number_of_attendants=0;
-                                        foreach ($value->users as $attendant)
-                                        {
-                                            $number_of_attendants++;
-                                        }
-                                    ?>
-                                    {{ Form::open(['route'=>'eventuser.store', 'method'=>'POST','files'=> true]) }}
-                                        <input type="hidden" name="event_id" value="{{ $value->id }}">
-                                        <div class="row">
-                                            <div class="col-md-4">
-                                                <button class="w3-button w3-green w3-round">Attend ({{ $number_of_attendants }})</button>
-                                            </div>
-                                            <div class="col-md-4">
-                                            <button class="w3-button w3-grey w3-round">Maybe ({{ $number_of_attendants }})</button>
-           
-                                            </div>
-                                            <div class="col-md-4">
-                                            <button class="w3-button w3-red w3-round">RSVP ({{ $number_of_attendants }})</button>
-                                            </div>
-                                        </div>
-                                        <br>
-                                            
-                                    {{ form::close() }}
-                                    
-                                    <!--<button  onclick="window.location.href = 'attendance';" class="w3-button w3-green w3-round"><h3>Attending ({{ $number_of_attendants }})</h3></button>
-                                    <button class="w3-button w3-grey w3-round"><h3>Maybe ({{ $number_of_attendants }})</h3></button>
-                                    <button class="w3-button w3-red w3-round"><h3>RSVP ({{ $number_of_attendants }})</h3></button>!-->
-                                    
-                                    
-                                    
-                                    <?php
-                                        $listcomments = $value->comments()->get();
-                                        
-                                    ?>
+  <!-- Grid -->
+  <div class="w3-row w3-padding w3-border">
 
-                                    @foreach ($listcomments as $listcomments) 
-                                    <?php
-                                        $commentwriters = $listcomments->user()->pluck('name');
-                                    ?>
-                                        <p><strong>{{ $commentwriters }}</strong> {{ $listcomments->comment }}</p>
-                                    @endforeach
-                                    
-                                    {{ Form::open(['route'=>'comments.store', 'method'=>'POST','files'=> true]) }}
-                                        <input type="hidden" name="event_id" value="{{ $value->id }}">
-
-                                        @include('comments.form_master')
-                                            
-                                    {{ form::close() }}
-                                    
-                                    
-                                
-                                </div>
-                        </div>
-                    </div>
-                            
-
-                    @endforeach
-                </div>
-                </div>
-       
-
-    </div>
+    <!-- Blog entries -->
+    <div class="w3-col l8 s12">
     
-    <div class="col-md-2">
-        
-        <div class="row pull-right">
-            <a href="{{route('events.create')}}" class="btn btn-success btn-sm glyphicon glyphicon-plus">Post Your Event</a>
-            @if ($message = Session::get('success'))
-            <div class="alert alert-success">
-                <p>{{ $message }}</p>
-            </div>
-            @endif
+      <!-- Blog entry -->
+        @foreach($event as $event => $value) 
+        <div class="w3-container w3-white w3-margin w3-padding-large">
+        <div class="w3-center">
+            
+            <h3>{{ $value->title }}</h3>
+            <h5>{{ $value->venue }} <span class="w3-opacity">{{ $value->date }}</span></h5>
         </div>
-        <br><br>
+
+        <div class="w3-justify">
+          <img src="{{url($value->image? 'uploads/'.$value->image:'images/noimage.jpg')}}" style="width:100%" class="w3-padding-16">
+          <p><strong>More Hats!</strong> {{ $value->description }}</p>
+          
+          {{ Form::open(['route'=>'events.store', 'method'=>'POST','files'=> true]) }}
+            <input type="hidden" name="condition" value=1>
+            <input type="hidden" name="event_id" value="{{ $value->id }}">
+            <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
+            <?php
+                $number_of_attendants=0;
+                foreach ($value->users as $attendant)
+                {
+                    $number_of_attendants++;
+                }
+            ?>
+
+            <p class="w3-left"><button class="w3-button w3-white w3-border" onclick="likeFunction(this)"><b><i class="fa fa-thumbs-up"></i>{{ $number_of_attendants }} Attending</b></button></p>
+          
+          {{ form::close() }}
+
+            <?php
+                $number_of_comments=0;
+                $listcomments = $value->comments()->get();
+                foreach ($listcomments as $com)
+                {
+                    $number_of_comments++;
+                }
+            ?>
             
-        <div class="row">            
-            <div class="w3-card-4">
-                <div class="w3-container w3-white">
-                    <h4><strong>Trending Events</strong></h4>
-                    <ul>
-                        <li>Shebby's Birthday Party</li><br>
-                        <li>trial2</li><br>
-                        <li>trial3</li><br>
-                    </ul>
-            
+                                    
+                     
+
+            <p class="w3-right"><button class="w3-button w3-black" onclick="myFunction('demo1')" id="myBtn"><b>Comments  </b> <span class="w3-tag w3-white">{{ $number_of_comments }}</span></button></p>
+            <p class="w3-clear"></p>
+            <div class="w3-row w3-margin-bottom" id="demo1" style="display:none">
+            <hr>
+                @foreach ($listcomments as $listcomments)
+                <?php
+                    $commentwriters = $listcomments->user()->pluck("name");
+                ?> 
+                
+                <div class="w3-col l10 m9">
+                    <h4>{{ $commentwriters }} <span class="w3-opacity w3-medium">{{ $listcomments->comment }}</span></h4>
+                    <p>May 3, 2015, 6:32 PM</p>
                 </div>
-            </div>
+                @endforeach
+                {{ Form::open(['route'=>'comments.store', 'method'=>'POST','files'=> true]) }}
+                    <input type="hidden" name="event_id" value="{{ $value->id }}">
+                    @include('comments.form_master')     
+                {{ form::close() }}
+          </div>
         </div>
-        <br>
-
-        <div class="row"> 
-        <div class="w3-card-4">
-        <div class="w3-container w3-white">
-            <h4><strong>Upcoming Events</strong></h4>
-            <ul>
-                <li>Shebby's Birthday Party</li><br>
-                <li>trial2</li><br>
-                <li>Ceremony of All</li><br>
-            </ul>
-            <br><hr>
-        </div></div>
-        </div>
-
-    </div> 
+      </div>
+      <hr>
+      @endforeach
     </div>
+
+    <!-- About/Information menu -->
+    <div class="w3-col l4">
+      <!-- About Card -->
+        <div class="w3-white w3-margin">
+            
+            <div class="w3-container w3-black">
+                <a href="{{route('events.create')}}"><h4><br>Post Your Event</h4></a>
+                <br>
+            </div>
+            <img src="images/img-02.png" alt="Jane" style="width:100%" class="w3-grayscale">
+        </div>
+      <hr>
+
+      <!-- Posts -->
+      <div class="w3-white w3-margin">
+        <div class="w3-container w3-padding w3-black">
+          <h4>Popular Events</h4>
+        </div>
+        <ul class="w3-ul w3-hoverable w3-white">
+          <li class="w3-padding-16">
+            <img src="/w3images/avatar_smoke.jpg" alt="Image" class="w3-left w3-margin-right" style="width:50px">
+            <span class="w3-large">Denim</span>
+            <br>
+            <span>Sed mattis nunc</span>
+          </li>
+          <li class="w3-padding-16">
+            <img src="/w3images/bandmember.jpg" alt="Image" class="w3-left w3-margin-right" style="width:50px">
+            <span class="w3-large">Sweaters</span>
+            <br>
+            <span>Praes tinci sed</span>
+          </li>
+          <li class="w3-padding-16">
+            <img src="/w3images/workshop.jpg" alt="Image" class="w3-left w3-margin-right" style="width:50px">
+            <span class="w3-large">Workshop</span>
+            <br>
+            <span>Ultricies congue</span>
+          </li>
+          <li class="w3-padding-16">
+            <img src="/w3images/avatar_smoke.jpg" alt="Image" class="w3-left w3-margin-right w3-sepia" style="width:50px">
+            <span class="w3-large">Trends</span>
+            <br>
+            <span>Lorem ipsum dipsum</span>
+          </li>
+        </ul>
+      </div>
+      <hr>
+
+      <!-- Advertising -->
+      <div class="w3-white w3-margin">
+        <div class="w3-container w3-padding w3-black">
+          <h4>Events Calendar</h4>
+        </div>
+        <div class="w3-container w3-white">
+          <div class="w3-container w3-display-container w3-light-grey w3-section" style="height:200px">
+            <span class="w3-display-middle">Calendar goes here</span>
+          </div>
+        </div>
+      </div>
+      <hr>
+
+
+
+      <!-- Inspiration -->
+      <div class="w3-white w3-margin">
+        <div class="w3-container w3-padding w3-black">
+          <h4>Advertisements</h4>
+        </div>
+        <div class="w3-row-padding w3-white">
+          <div class="w3-col s6">
+            <p><img src="/w3images/jeans.jpg" alt="Jeans" style="width:100%"></p>
+            <p><img src="/w3images/team1.jpg" alt="Jeans" style="width:100%"></p>
+          </div>
+          <div class="w3-col s6">
+            <p><img src="/w3images/avatar_hat.jpg" alt="Men in Hats" style="width:100%" class="w3-grayscale"></p>
+            <p><img src="/w3images/team4.jpg" alt="Jeans" style="width:100%"></p>
+         </div>
+        </div>
+      </div>
+      <hr>
+
+    <!-- END About/Intro Menu -->
+    </div>
+
+  <!-- END GRID -->
+  </div>
+
+<!-- END w3-content -->
 </div>
+<footer class="w3-container w3-dark-grey" style="padding:32px">
+  <a href="#" class="w3-button w3-black w3-padding-large w3-margin-bottom"><i class="fa fa-arrow-up w3-margin-right"></i>To the top</a>
+</footer>
 @endsection
