4b27c052ffee4927d0112e687430d6ff6dc7243d
Buthaina <buthainasudi@gmail.com>
2019-10-12 14:40:53 +0800

    Rename the pivot table to Attendance

--- app/User.php
+++ app/User.php
@@ -55,6 +55,6 @@ class User extends Authenticatable
 
     public function events()
     {
-        return $this->belongsToMany('App\Event');
+        return $this->belongsToMany('App\Event', 'attendance');
     }
 }
