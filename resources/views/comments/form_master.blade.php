f4c597ca4af4c966367e6a385530af5e0cc4e4b9
Buthaina <buthainasudi@gmail.com>
2019-10-12 14:45:29 +0800

    Update comments interface

--- resources/views/comments/form_master.blade.php
+++ resources/views/comments/form_master.blade.php
@@ -1,15 +1,8 @@
-    <div class="row justify-content-center">
-   
 
-            <div class="col-md-8">
-                <div class="form-group {{ $errors->has('title') ? 'has-error' : "" }}">
+                <div class="w3-col l10 m9">
                     {{ Form::text('comment',NULL, ['class'=>'form-control', 'id'=>'comment', 'placeholder'=>' Add Comments']) }}
                     {{ $errors->first('comment', '<p class="help-block">:message</p>') }}
                 </div>
-            </div>
-
-            <div class="col-md-4">
-                {{ Form::button(isset($model)? 'Comment' : 'Comment' , ['class'=>'btn btn-primary', 'type'=>'submit']) }}
-            </div>
-
-    </div>
+                <div class="w3-col l2 m3">
+                    {{ Form::button(isset($model)? 'Comment' : 'Comment' , ['class'=>'btn btn-primary w3-right', 'type'=>'submit']) }}
+                </div>
